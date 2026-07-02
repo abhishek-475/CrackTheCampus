d471954ef7e44f53916a5753809c36cb0ca51bb0
Abhishek Pavithran <abhishekpavi6@gmail.com>
2026-07-02 11:25:37 +0530

    architecture  update

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,11 @@ import "./index.css";
 import App from "./App.jsx";
 
 import { ThemeProvider } from "./context/ThemeContext.jsx";
-import { UIProvider } from "./context/UIContext.jsx";
-import { ScoreProvider } from "./context/ScoreContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
-      <UIProvider>
-        <ScoreProvider>
           <App />
-        </ScoreProvider>
-      </UIProvider>
     </ThemeProvider>
   </StrictMode>
 );
