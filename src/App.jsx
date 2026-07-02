5e52771f22f8981b7bbfc01a123ea555f0c6318f
Abhishek Pavithran <abhishekpavi6@gmail.com>
2026-07-02 13:38:15 +0530

    clean up unwanted

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="min-h-screen overflow-x-hidden bg-slate-50 dark:bg-slate-900">
       <Header />
-
+      
       <main id="main-content">
         <Hero />
         <Companies />
